e88ca5dfdc770f8b82fff4e5121460d9e0b81777
wbisque <wbisque@mines.edu>
2022-09-27 14:59:03 -0600

    changed head dimension and eye_y

--- src/Cat.java
+++ src/Cat.java
@@ -5,9 +5,9 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 150;
+	private static final int HEAD_DIMENSION = 130;
 	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
+	private static final int EYE_Y = HEAD_DIMENSION/2;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/4;
 	// pick eye dimensions
